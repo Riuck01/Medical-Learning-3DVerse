e05e9dc5fdf67c11fe68dd40556a5971337b2d70
arthus <arthus@meuret.net>
2023-12-06 16:33:56 +0100

    React App settup and config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const publicToken = "public_WF4PaNcaru0ljGaO";
-export const mainSceneUUID = "17a9f35d-ff5d-4130-b7e6-ede81935b424";
-export const characterControllerSceneUUID = "25d1b08b-773a-4f38-935f-7ffcecd49a48";
+export const publicToken = "public_kDhOykNV4SFk_GVO";
+export const mainSceneUUID = "2ebfc8b2-2107-4fcc-8d3f-da7d9cd67725";
+export const characterControllerSceneUUID = "33d900f9-1eac-4fec-83d1-d9af3be7f331";
