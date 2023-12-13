60bcc739f36461156ea753d89da60aabc6f31f1c
Riuck01 <tforest@gaming.tech>
2023-12-13 12:17:16 +0100

    commit modal + overlay

--- src/HUD.jsx
+++ src/HUD.jsx
@@ -1,11 +1,44 @@
-import './HUD.css'
+ 
+import React, { useState } from 'react';
+import './HUD.css';
 
-export function HUD() {
+export const HUD = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const openModal = () => {
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
   return (
     <div className='HUD'>
-      <div>
-        <h1>This text is printed on top of the scene's canvas</h1>
-      </div>
+      <button onClick={openModal}>Open Modal</button>
+
+      {modalVisible && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <span className="close-button" onClick={closeModal}>&times;</span>
+            <h2>Fiche patient</h2>
+            <p>Nom: x prénom: x</p>
+            <p>Age...</p>
+            <p>problèmes</p>
+            <p>Toutes infos importantes</p>
+            <p>Nom: x prénom: x</p>
+            <p>Age...</p>
+            <p>problèmes</p>
+            <p>Toutes infos importantes</p>
+            <p>Nom: x prénom: x</p>
+            <p>Age...</p>
+            <p>problèmes</p>
+            <p>Toutes infos importantes</p>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
+
+export default HUD; 
