7075d9e4aff2cfcccc407a374b3f21782e374cac
Riuck01 <tforest@gaming.tech>
2023-12-15 17:14:09 +0100

    un peu de css ;)

--- src/PDF.jsx
+++ src/PDF.jsx
@@ -8,7 +8,7 @@ export const PDF = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [numPages, setNumPages] = useState(null);
   const [error, setError] = useState(null);
-  const [pdfUrl, setPdfUrl] = useState('./TEST3.pdf'); // Initial PDF file
+  const [pdfUrl, setPdfUrl] = useState('./TEST3.pdf');
 
   const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
