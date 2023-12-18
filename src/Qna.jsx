893dc3aeab6c9544224fe8d93461a2c3ab942e5a
arthus <arthus@meuret.net>
2023-12-18 18:31:51 +0100

    qna working

--- src/Qna.jsx
+++ src/Qna.jsx
@@ -2,6 +2,8 @@ import './qna.css'
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
 
 export function Qna() {
   const [open, setShow] = useState(false);
@@ -14,6 +16,27 @@ export function Qna() {
     {title: "Tom va-t-il gagner sa game ?", a1: "non", a2: "non", a3: "non", a4: "non", },
   ];
 
+  const answers = {
+    0: '',
+  };
+  
+  const nextQuestion = (answer) => {
+    answers[curQuestionIndex] = answer;
+    if (curQuestionIndex<questions.length-1) setCount(curQuestionIndex+1)
+    changeAnswer();
+  };
+
+  const prevQuestion = () => {
+    if (curQuestionIndex>0) setCount(curQuestionIndex-1)
+  };
+
+  const changeAnswer = () => {
+    if (document.getElementById('answer1').checked) answers[curQuestionIndex] = "1";
+    if (document.getElementById('answer2').checked) answers[curQuestionIndex] = "2";
+    if (document.getElementById('answer3').checked) answers[curQuestionIndex] = "3";
+    if (document.getElementById('answer4').checked) answers[curQuestionIndex] = "4";
+  };
+
   return (
     <div className='qna-container'>
       <Button variant="primary" onClick={openForm}>Questionnaire</Button>
@@ -24,16 +47,16 @@ export function Qna() {
         <Modal.Body>
           <Modal.Title>{questions[curQuestionIndex].title}</Modal.Title>
           <br />
-          <ul>
-            <li> {questions[curQuestionIndex].a1} </li>
-            <li> {questions[curQuestionIndex].a2} </li>
-            <li> {questions[curQuestionIndex].a3} </li>
-            <li> {questions[curQuestionIndex].a4} </li>
-          </ul>
+          <Form.Group>
+            <Form.Check type={'radio'} id='answer1' name='answer' label={questions[curQuestionIndex].a1}/>
+            <Form.Check type={'radio'} id='answer2' name='answer' label={questions[curQuestionIndex].a2}/>
+            <Form.Check type={'radio'} id='answer3' name='answer' label={questions[curQuestionIndex].a3}/>
+            <Form.Check type={'radio'} id='answer4' name='answer' label={questions[curQuestionIndex].a4}/>
+          </Form.Group>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="primary" onClick={() => {if (curQuestionIndex>0) setCount(curQuestionIndex-1)}}>Précédant</Button>
-          <Button variant="primary" onClick={() => {if (curQuestionIndex<questions.length-1) setCount(curQuestionIndex+1)}}>Suivant</Button>
+          <Button variant="primary" onClick={prevQuestion}>Précédant</Button>
+          <Button variant="primary" type='submit' onClick={nextQuestion}>Suivant</Button>
         </Modal.Footer>
       </Modal>
     </div>
