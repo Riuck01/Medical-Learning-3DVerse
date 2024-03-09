3ed8243235d36112a91b54b0af960e5dfa6e8580
Riuck01 <tforest@gaming.tech>
2024-03-09 15:26:56 +0100

    uid

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const publicToken = "public_kDhOykNV4SFk_GVO";
-export const mainSceneUUID = "2ebfc8b2-2107-4fcc-8d3f-da7d9cd67725";
+export const mainSceneUUID = "e2d5c376-8b40-471a-8108-ba8796c5a30a";
 export const characterControllerSceneUUID = "33d900f9-1eac-4fec-83d1-d9af3be7f331";
